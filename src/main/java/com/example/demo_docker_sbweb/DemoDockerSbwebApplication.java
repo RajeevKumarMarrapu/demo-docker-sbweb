b6110b9f967ce93fe467ed14206e68fff8fcc7e1
rajeev <vdr_vdr_rajeev@airasia.com>
2025-03-16 01:24:38 +0530

    initial commit

---
+++ src/main/java/com/example/demo_docker_sbweb/DemoDockerSbwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_docker_sbweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerSbwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDockerSbwebApplication.class, args);
+	}
+
+}
